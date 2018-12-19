bc21835818ffd2f4d85050ea6d5ebb2e188f4745
KamyllaAlmeida <kamylla.florencio@gmail.com>
2018-12-19 21:46:56 +0000

    Creating routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,14 +1,23 @@
 Rails.application.routes.draw do
-  resources :stories
-  resources :stories_likes
-  resources :genre_stories
-  resources :genres
-  resources :feedback_likes
-  resources :feedbacks
-  resources :segments
-  resources :comments
-  resources :followers
-  resources :users
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'stories#index'
+
+  namespace :api do 
+    resources   :stories do
+      resources :stories_likes, only: [:create]
+      post      :read
+    end
+    resources   :feedbacks, only: [:new, :create, :destroy ] do 
+      resources :feedbacks_likes, only: [:create]
+    end
+    resources :users do 
+      resources :followers, only: [:create, :destroy]
+      get     :draft
+      post    :draft
+    end
+    resources :comments, only: [:new, :create, :destroy ] do 
+      resources :comments_likes, only: [:create]
+    end
+  end
+
 end
