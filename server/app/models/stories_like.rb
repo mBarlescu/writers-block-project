170a649d895813f32f74a21b14bd42a491cd5536
Keishon Ross <keishonross97@gmail.com>
2018-12-19 22:51:46 +0000

    add relationships between tables to each model, create core instances(users, stories, genres) in seed file

--- server/app/models/stories_like.rb
+++ server/app/models/stories_like.rb
@@ -1,2 +1,4 @@
 class StoriesLike < ApplicationRecord
+  belongs_to :user
+  belongs_to :story
 end
--- server/db/seeds.rb
+++ server/db/seeds.rb
@@ -10,25 +10,10 @@
 
 puts "Creating seed file..."
 
-
-puts "Making Genres..."
-
-genre1 = Genre.find_or_create_by! name: 'Fantasy'
-genre2 = Genre.find_or_create_by! name: 'Romance'
-genre3 = Genre.find_or_create_by! name: 'Sci-Fi'
-
-puts "Making Stories..."
-
-story1 = genre1.stories.find_or_create_by! name: "Wow I'm A Real Book!"
-story2 = genre2.stories.find_or_create_by! name: "My Relationship With The Vacuum of Space"
-story3 = genre3.stories.find_or_create_by! name: "Robots Will Totally Kill Us All Guys"
-
-
-
 puts "Making Users..."
 
 
-user1 = User.find_or_create_by!(
+joe = User.find_or_create_by!(
   first_name: "Joe",
   last_name: "Dolan",
   description: "",
@@ -37,7 +22,7 @@ user1 = User.find_or_create_by!(
   image: "https://daphoto.com/wp-content/uploads/2013/11/Friel_4065-5x7x300(pp_w642_h900).jpg"
 )
 
-user2 = User.find_or_create_by!(
+ben = User.find_or_create_by!(
   first_name: "Ben",
   last_name: "Jenoli",
   description: "",
@@ -47,11 +32,46 @@ user2 = User.find_or_create_by!(
   
 )
 
-user3 = User.find_or_create_by!(
+kim = User.find_or_create_by!(
   first_name: "Kim",
   last_name: "Kennedy",
   description: "",
   email: "kim@gmail.com",
   password: "aaa",
   image: "https://i0.wp.com/blog.scottrklinephoto.com/wp-content/uploads/2013/10/Amy_Wigdahl_Headshot_15E9688.jpg"
+)
+
+puts "Making Genres..."
+
+genre1 = Genre.find_or_create_by! name: 'Fantasy'
+genre2 = Genre.find_or_create_by! name: 'Romance'
+genre3 = Genre.find_or_create_by! name: 'Sci-Fi'
+
+puts "Making Stories..."
+
+Story.create!(
+  user_id: joe.id,
+  title: "The Many Hardships of Hard Ships",
+  description: "A touching story that gives you a relatable window into the mind of a master Shipsmith.",
+  text: "Here is where the story doc is kept",
+  image: "https://images.alphacoders.com/450/450769.jpg",
+  published: true
+)
+
+Story.create!(
+  user_id: ben.id,
+  title: "Willy and the Fenk",
+  description: "A touching story that gives you a relatable window into the mind of a master Fenk and his companions.",
+  text: "Here is where the story doc is kept",
+  image: "http://orig06.deviantart.net/1d07/f/2015/103/8/d/the_rake_by_tsabo6-d8pk6q9.jpg",
+  published: true
+)
+
+Story.create!(
+  user_id: kim.id,
+  title: "The Darkness Within",
+  description: "A touching story that gives you a relatable window into the mind of the author and their ability to become very dark very quickly.",
+  text: "Here is where the story doc is kept",
+  image: "http://eskipaper.com/images/darkness-master-1.jpg",
+  published: true
 )
