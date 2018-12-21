fb6edf1fd59f285a7a5dc58d5c4003be11301505
Mark Barlescu <barlescu92@gmail.com>
2018-12-21 21:26:03 +0000

    users/:id now works properly, had bugs prior

--- client/src/components/Users/UserPage.jsx
+++ client/src/components/Users/UserPage.jsx
@@ -1,73 +1,64 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import User from './User'
+import RouteError from '../Errors/RouteError'
 
 const UsersPage = (props) => {
 
-  if (!props.stories[`${props.match.params.id}`]) {
-    return null;
+  let userId = props.match.params.id
+  const userIdInt = Number.parseInt(userId)
+  const allStories=props.stories
+  const allUsers=props.users
+
+
+  allUsers.sort(function(a, b) {
+    return a.id - b.id
+  })
+
+  allStories.sort(function (a, b) {
+    return a.user_id - b.user_id
+  })
+
+  const user_binary_search = (arr, value) => {
+    let high = arr.length - 1;
+    let low = 0;
+    let mid = 0;
+
+    while ( low <= high ) {
+      mid = Math.floor((high + low) / 2)
+      if(arr[mid].id === value){
+        return arr[mid];
+      } else if (value > arr[mid].id){
+        low = mid + 1;
+      } else {
+        high =mid - 1;
+      }
+    }
+    return -1
   }
 
-    let userId = props.match.params.id
-    console.log(typeof userId);
-    const userIdInt = Number.parseInt(userId)
-    console.log(typeof userIdInt)
-    console.log(userIdInt)
-    const allStories=props.stories
-    const allUsers=props.users
-    console.log(props)
+  const story_binary_search = (arr, value) => {
+    let high = arr.length - 1;
+    let low = 0;
+    let mid = 0;
 
-    allUsers.sort(function(a, b) {
-      return a.id - b.id
-    })
-
-    allStories.sort(function (a, b) {
-      return a.user_id - b.user_id
-    })
-
-    console.log('sroted list of users based on their id', allUsers)
-
-    const user_binary_search = (arr, value) => {
-      let high = arr.length - 1;
-      let low = 0;
-      let mid = 0;
-
-      while ( low <= high ) {
-        mid = Math.floor((high + low) / 2)
-        if(arr[mid].id === value){
-          return arr[mid];
-        } else if (value > arr[mid].id){
-          low = mid + 1;
-        } else {
-          high =mid - 1;
-        }
+    while ( low <= high ) {
+      mid = Math.floor((high + low) / 2)
+      if(arr[mid].user_id === value){
+        return arr[mid];
+      } else if (value > arr[mid].id){
+        low = mid + 1;
+      } else {
+        high =mid - 1;
       }
-      return -1
     }
-
-    const story_binary_search = (arr, value) => {
-      let high = arr.length - 1;
-      let low = 0;
-      let mid = 0;
-
-      while ( low <= high ) {
-        mid = Math.floor((high + low) / 2)
-        if(arr[mid].user_id === value){
-          return arr[mid];
-        } else if (value > arr[mid].id){
-          low = mid + 1;
-        } else {
-          high =mid - 1;
-        }
-      }
-      return -1
-    }
-
-    console.log('binary search for a user based on their id', user_binary_search(allUsers, userIdInt))
-    console.log('binary search for a story based on their id', story_binary_search(allStories, userIdInt))
+    return -1
+  }
 
 
-
+  if (user_binary_search(allUsers, userIdInt) === -1) {
+    return <RouteError />;
+  }
 
   return (
     <div className='container my-container'>
