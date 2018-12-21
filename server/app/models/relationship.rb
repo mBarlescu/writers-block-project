eb66fc5ba8cf68bce2c566c1ec8ab38a82ba47c1
Keishon Ross <keishonross97@gmail.com>
2018-12-21 02:03:29 +0000

    users can follow other users, add validation, deleting user -> cascades: confirmed

--- server/app/models/relationship.rb
+++ server/app/models/relationship.rb
@@ -1,4 +1,10 @@
 class Relationship < ApplicationRecord
   belongs_to :follower, class_name: "User", foreign_key: "follower_id"
   belongs_to :following, class_name: "User", foreign_key: "following_id"
+
+  validates :follower_id, presence: true
+  validates :following_id, presence: true
+
+
+  validates :follower_id, uniqueness: { scope: :following_id, message: "ALREADY EXISTS! Sorry, please follow someone else." }
 end
