dd03d80aa611fb202f17b251d0d2999cbd980d8a
Mark Barlescu <barlescu92@gmail.com>
2018-12-28 04:02:51 +0000

    the put/post requests on createPage now are being sent

--- client/src/components/Stories/CreateStory.jsx
+++ client/src/components/Stories/CreateStory.jsx
@@ -1,21 +1,88 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 class CreateStory extends Component {
+
   constructor(props){
 
     super(props);
 
-    let storyId = props.match.params.id
-    const storyIdInt = Number.parseInt(storyId)
-    console.log(props)
+    this.state={
+      story: {},
+      text: ''
+    }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+
+
+
+
+    let storyId = props.match.params.id;
+    const storyIdInt = Number.parseInt(storyId);
+    console.log(props);
+
+
+    axios.get(`http://localhost:3000/api/stories/${storyId}/new`)
+    .then(res => {
+      console.log("NEW", res)
+      this.setState({story: res.data})
+      console.log('STATE', this.state)
+    })
+    .catch(err => {
+      console.log('AAAAAAAA', err)
+    })
+
+    console.log('state', this.state)
+
+
+
 
   }
+
+   handleChange(event) {
+    this.setState({ text: event.target.value });
+    console.log('EVENT', event)
+  };
+
+  handleSubmit(event){
+    event.preventDefault();
+
+  console.log('Handling submit', this);
+  const text = {
+    text: this.state.text
+  }
+
+  let storyId = this.state.story.id;
+
+  axios.put(`http://localhost:3000/api/stories/${storyId}`, { text })
+    .then(res => {
+      console.log('CONTENT2', res);
+      console.log('CONTENT', res.data);
+    })
+
+  }
+
 
 
   render(){
 
     return (
-      <p> hello, this is the CreateStory page </p>
+      <div>
+        <br />
+        <br />
+        <div className='text-center'>{this.state.story.title} </div>
+          <br />
+          <br />
+        <div className='createContainer'>
+          <form onSubmit={this.handleSubmit}>
+
+              <input type='text' name='text' onChange={this.handleChange} />
+
+            <button type='submit'>Save</button>
+          </form>
+        </div>
+      </div>
     )
   }
 }
